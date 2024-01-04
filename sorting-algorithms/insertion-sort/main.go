78c4b5e6415c9f097a9b295121b756d254cb936b
fredele20 <victorisholaoladele@gmail.com>
2024-01-04 17:33:45 +0100

    insertion sort implementation

---
+++ sorting-algorithms/insertion-sort/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+
+func insertionSort(arr []int) []int {
+
+	for i:=1; i < len(arr); i++ {
+		for j := i; j > 0 && arr[j-1] > arr[j]; j-- {
+			arr[j-1], arr[j] = arr[j], arr[j-1]
+		}
+		fmt.Println(arr)
+	}
+
+	return arr
+}
+
+func main() {
+	fmt.Println(insertionSort([]int{2, 5, 3, 6, 9, 1, 4}))
+}
